426979a59e014833888f89a6914d8c6e98988e2d
MarinaMeyta <somekindofcookiemonster@gmail.com>
2015-07-24 22:35:51 +0600

    made patterns for different pages

--- bt_app/views.py
+++ bt_app/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request, 'base_index.html', {})
+
+def about_page(request):
+   	return render(request, 'base_about.html', {})
+
+def contacts_page(request):
+   	return render(request, 'base_contacts.html', {})
--- bt_site/urls.py
+++ bt_site/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    url(r'^$', 'bt_app.views.home_page', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'bt_app.views.home_page', name='home'),
+    url(r'^about/', 'bt_app.views.about_page', name='about'),
+    url(r'^contacts/', 'bt_app.views.contacts_page', name='contacts'),
 ]
