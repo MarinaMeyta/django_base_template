b8ef78e74b115ef2cbf3f73c06cf5bca09cc1cec
MarinaMeyta <somekindofcookiemonster@gmail.com>
2015-07-28 17:49:03 +0600

    added Person model

--- bt_app/models.py
+++ bt_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class  Person(models.Model):
+	name = models.TextField(max_length=100, verbose_name="Name")
+	surname = models.TextField(max_length=100, verbose_name="Surname")
+		
--- bt_app/views.py
+++ bt_app/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
+from bt_app.models import Person
+from django_tables2 import RequestConfig
+from bt_app.tables import PersonTable
 
 # Create your views here.
 def home_page(request):
     return render(request, 'base_index.html', {})
 
+def people_page(request):
+    table = PersonTable(Person.objects.all())
+    RequestConfig(request).configure(table)
+    return render(request, 'base_people.html', {'table': table})
+
 def about_page(request):
-   	return render(request, 'base_about.html', {})
+    return render(request, 'base_about.html', {})
 
 def contacts_page(request):
-   	return render(request, 'base_contacts.html', {})
+    return render(request, 'base_contacts.html', {})
+
+    
