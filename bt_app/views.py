186af8f2ab1fdd8ed2abebc5d044b01d93f236bb
MarinaMeyta <somekindofcookiemonster@gmail.com>
2015-08-06 22:10:31 +0600

    workind POST-request

--- bt_app/views.py
+++ bt_app/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from bt_app.models import Person
 from django_tables2 import RequestConfig
 from bt_app.tables import PersonTable
+from django.core.context_processors import csrf
 
 from django.http import HttpResponse
+
+from django.views.decorators.csrf import requires_csrf_token
+from django.shortcuts import render
+
+
+from django.template import RequestContext
 
 # Create your views here.
 def home_page(request):
@@ -25,10 +32,22 @@ def contacts_page(request):
 
 
 def add_person(request):
-    if request.method == "POST":
-        person = request.POST['new_person']
-    else:
-        person = ''
+    print ('request: ', request)
 
-    html = render('base_people.html', {'table': table})
-    return HttpResponse(html)
+    #c = {}
+    #c.update(csrf(request))
+
+    if request.method == "POST":
+        # person = request.POST['new_person']
+        new_person_form = PersonForm(request.POST)
+        if new_person_form.is_valid():
+            new_person_form.save()
+            # html = render('base_people.html', {'table': table}, context_instance=RequestContext(request))
+            return render('base_people.html', {'table': table}, c)
+            #return render_to_response('base_people.html', {'table': table})
+    # else:
+    #     person = ''
+
+    #return render('base_people.html', c)
+
+    
--- bt_site/urls.py
+++ bt_site/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from bt_app.views import add_person
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'bt_app.views.home_page', name='home'),
@@ -8,5 +10,6 @@ urlpatterns = [
     url(r'^about/', 'bt_app.views.about_page', name='about'),
     url(r'^contacts/', 'bt_app.views.contacts_page', name='contacts'),
 
-    url(r'^add_person/$', 'bt_app.views.add_person', name='add_person'),
+    #url(r'^add_person/$', 'bt_app.views.add_person', name='add_person'),
+    url(r'^add_person/$', add_person, name='add_person'),
 ]
